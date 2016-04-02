cb8d53d6b1af3c7db6670cc901e1fcbe0faabdb8
Jeiti <jt2349311>
2016-04-02 09:59:49 +0300

    Добавили загрузку изображений к новости closes #2400 @20m

--- news_add.php
+++ news_add.php
@@ -1,8 +1,13 @@
 <?php
+print_r($_FILES);
 if (isset($_POST["hotnews"])){
     $link=mysqli_connect("localhost", "root", "123", "insuarance");
-    if (!mysqli_query($link,"insert into news(title, content) values ('$_POST[title]', '$_POST[content]')")){
+    if (!mysqli_query($link,"insert into news(title, content, picture) values ('$_POST[title]', '$_POST[content]', '" . $_FILES["picture"]["name"] . "')")){
         echo mysqli_error($link);
+    }
+    else{
+        $filename = __DIR__."/img/news_img/" . $_FILES["picture"]["name"];
+        move_uploaded_file($_FILES["picture"]["tmp_name"], $filename);
     }
     mysqli_close($link);
 }
@@ -10,7 +15,7 @@ if (isset($_POST["hotnews"])){
 require_once ("header.php");
 ?>
 
-<form class="" role="form" action="news_add.php" method="post">
+<form class="" role="form" action="news_add.php" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="exampleInputEmail1">title</label>
         <input class="form-control" id="exampleInputEmail1" placeholder="title" type="text" name="title">
@@ -21,7 +26,7 @@ require_once ("header.php");
     </div>
     <div class="form-group">
         <label for="exampleInputFile">File input</label>
-        <input id="exampleInputFile" type="file">
+        <input id="exampleInputFile" type="file" name="picture">
         <p class="help-block">Example block-level help text here.</p>
     </div>
     <button type="submit" class="btn btn-default" name="hotnews">Submit</button>
