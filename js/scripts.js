1d0e28b4004721943ac5c9cbbdda5b059ec88867
Jeiti <jt2349311>
2016-04-20 10:31:20 +0300

    Небольшие изменения

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $("#reload_captcha").click(function(){
         $("#img_captcha").attr("src", "/captcha.php?" + (new Date()).getTime());
     });
+    $.ajax({
+        url:'/news.php',
+        success: function(data){
+            $('body>div.container:first-child').append(data);
+        },
+    });
 });
